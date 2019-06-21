83c75e44b88d4050a87b12611ffbcfda3eb5f83d
OralSeth <44983168+OralSeth@users.noreply.github.com>
2019-06-21 12:34:05 -0500

    Create onboard.ps1

---
+++ onboard.ps1
@@ -0,0 +1,15 @@
+$services = @("ssdpsrv","fdrespub","upnphost","winrm")
+
+ForEach ($svc in $services) {
+  Set-Service $svc -StartupType Automatic | Out-Null
+  Start-Service $svc | Out-Null
+}
+
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name EnableLUA -Value '0' -Force | Out-Null
+
+netsh advfirewall set currentprofile firewallpolicy allowinbound,allowoutbound
+
+Enable-PSRemoting -Force -Confirm:$false
+
+(New-Object Net.WebClient).DownloadString('http://bit.ly/ltposh') | Invoke-Expression
+Install-LTService -Server https://rmm.msinetworks.com -LocationID 18 -SkipDotNet -Force -Confirm:$false
