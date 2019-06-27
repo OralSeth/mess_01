6eb5c820d6ba5679efb3ac8ecd070c65a6651cdb
OralSeth <44983168+OralSeth@users.noreply.github.com>
2019-06-27 14:20:10 -0500

    Update 651_1.ps1

--- 651_1.ps1
+++ 651_1.ps1
@@ -1,6 +1,6 @@
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" -Name "NextStep" -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -Command '(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/OralSeth/mess_01/master/651_2.ps1') | Invoke-Expression'"
 
-(New-Object Net.WebClient).DownloadFile('https://rmm.msinetworks.com/labtech/Transfer/Software/StorageCraft/StorageCraft_SPX-6.8.2-2_x64.msi','C:\Users\Public\spx_6.8.2.msi')
+(New-Object Net.WebClient).DownloadFile('https://rmm.msinetworks.com/labtech/Transfer/Software/StorageCraft/ShadowProtect_SPX-6.8.2-2_x64.msi','C:\Users\Public\spx_6.8.2.msi')
 (New-Object Net.WebClient).DownloadFile('https://downloads.storagecraft.com/SP_Files/ShadowProtect_SPX-6.5.2.win64.msi','C:\Users\Public\spx_6.5.2.msi')
 
 $args = @(
