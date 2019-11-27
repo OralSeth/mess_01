68fdeeeb3000f7ee1dd6d2ef0758e2de85505c17
OralSeth <44983168+OralSeth@users.noreply.github.com>
2019-11-27 15:01:46 -0600

    Update spx684.ps1

--- spx684.ps1
+++ spx684.ps1
@@ -1,5 +1,7 @@
-(New-Object Net.WebClient).DownloadFile('https://downloads.storagecraft.com/SP_Files/ShadowProtect_SPX-6.8.4-5_x64.msi','C:\Users\Public\SPX_6.8.4.msi')
-
+$ErrorActionPreference = 'SilentlyContinue'
+If (!(Test-Path 'C:\Users\Public\SPX_6.8.4.msi')) {
+  (New-Object Net.WebClient).DownloadFile('https://downloads.storagecraft.com/SP_Files/ShadowProtect_SPX-6.8.4-5_x64.msi','C:\Users\Public\SPX_6.8.4.msi')
+}
 $sArgs = "/qn /package C:\Users\Public\SPX_6.8.4.msi IACCEPT=STORAGECRAFT.EULA"
 
 $RDP = Get-WmiObject -Namespace "root\CIMV2\TerminalSevices" -Class "Win32_TerminalServiceSetting" | Select -ExpandProperty TerminalServerMode
