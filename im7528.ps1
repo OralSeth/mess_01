3ca2e5c031ff1b0d639f8101cd4dea3df4984b66
OralSeth <44983168+OralSeth@users.noreply.github.com>
2019-11-29 16:20:58 -0600

    Update im7528.ps1

--- im7528.ps1
+++ im7528.ps1
@@ -1,17 +1,20 @@
 $imPath = "C:\Program Files (x86)\StorageCraft\ImageManager"
-  
-If (Test-Path $imPath) {
+$imVersion = (Get-WmiObject Win32_Product | Select Name,Version | Where-Object {$_.Name -like '*ImageManager*'}).Version
 
-  If (!(Test-Path "$($imPath)\Configuration Backups")) {
-    New-Item -Path "$($imPath)" -Name "Configuration Backups" -ItemType Directory | Out-Null
+If ($imVersion -ne '7.5.28') {  
+  If (Test-Path $imPath) {
+
+    If (!(Test-Path "$($imPath)\Configuration Backups")) {
+      New-Item -Path "$($imPath)" -Name "Configuration Backups" -ItemType Directory | Out-Null
+    }
+
+    Copy-Item "$($imPath)\ImageManager.mdb" "$($imPath)\Configuration Backups\ImageManager.mdb"
+    & cmd.exe /c "REG EXPORT `"HKLM\SYSTEM\CurrentControlSet\Services\StorageCraft ImageManager`" `"$($imPath)\Configuration Backups\ImageManager.reg`""
   }
 
-  Copy-Item "$($imPath)\ImageManager.mdb" "$($imPath)\Configuration Backups\ImageManager.mdb"
-  & cmd.exe /c "REG EXPORT `"HKLM\SYSTEM\CurrentControlSet\Services\StorageCraft ImageManager`" `"$($imPath)\Configuration Backups\ImageManager.reg`""
+  If (!(Test-Path "C:\Users\Public\IM_7.5.28.exe")) {
+      (New-Object Net.WebClient).DownloadFile('https://downloads.storagecraft.com/SP_Files/ImageManager_Setup_7.5.28.exe','C:\Users\Public\IM_7.5.28.exe')
+    }
+
+  & cmd.exe /c "C:\Users\Public\IM_7.5.28.exe /quiet IACCEPT=STORAGECRAFT.EULA Reboot=Yes"
 }
-
-If (!(Test-Path "C:\Users\Public\IM_7.5.28.exe")) {
-    (New-Object Net.WebClient).DownloadFile('https://downloads.storagecraft.com/SP_Files/ImageManager_Setup_7.5.28.exe','C:\Users\Public\IM_7.5.28.exe')
-  }
-
-& cmd.exe /c "C:\Users\Public\IM_7.5.28.exe /quiet IACCEPT=STORAGECRAFT.EULA Reboot=Yes"
